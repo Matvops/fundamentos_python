491aa595ea90227f54f4e3e418b3f38338d5e053
Matheus <matheusmartinscia@gmail.com>
2025-06-10 20:54:57 -0300

    Hello, world

---
+++ Main.py
@@ -0,0 +1,5 @@
+print('Hello, world!')
+dia = input("Que dia você nasceu?")
+mes = input("Que mês você nasceu?")
+ano = input("Que ano você nasceu?")
+print('DATA DE NASCIMENTO:', dia, '/', mes, '/', ano)
