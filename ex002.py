d43b4bf75630635a4ff1ebbc68f74a57829698fd
Matheus <matheusmartinscia@gmail.com>
2025-06-10 21:34:56 -0300

    Feat: Primeiros exercícios

---
+++ ex002.py
@@ -0,0 +1,11 @@
+print('--------------------- ANTECESSOR E SUCESSOR ---------------------')
+num = int(input("Digite um número: "))
+
+antecessor = num - 1
+sucessor = num + 1
+
+
+
+print("O antecessor de {} é {}.".format(num, antecessor))
+print("O sucessor de {} é {}.".format(num, sucessor))
+
